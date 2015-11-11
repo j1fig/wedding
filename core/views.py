81741cbffe3e7d957a3cba7a07488116bc46fdc1
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2015-11-11 19:19:56 +0000

    added base confirmation

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
+from django.http import JsonResponse
 
 from core.forms import GuestForm
 
@@ -8,3 +9,16 @@ class GuestAdd(FormView):
     form_class = GuestForm
     success_url = reverse_lazy('home:home')
     template_name = 'core/add_guest.html'
+
+    def form_valid(self, form):
+        form.save()
+        context = {
+            'next': self.get_success_url(),
+            'success': True
+        }
+        return JsonResponse(context)
+
+    def form_invalid(self, form):
+        response = self.render_to_response(self.get_context_data(form=form))
+        form_html = response.render().content
+        return JsonResponse({'success': False, 'form_html': form_html})
--- home/static/home/js/confirm.js
+++ home/static/home/js/confirm.js
@@ -1,9 +1,48 @@
 var Confirm = (function () {
     var _initialized = false;
     var _form_html;
+    var _confirm_container;
+    var _form_index = 0;
 
-    var _initForm = function () {
-      console.log(_form_html);
+    var addForm = function () {
+      var form = _confirm_container
+        .insert('div', ':first-child')
+        .attr('id', 'form_'+_form_index)
+        .classed('col-xs-4 form-container', true);
+      _form_index++;
+
+      console.log(form);
+      _displayForm(form, _form_html);
+    }
+
+    var _displayForm = function (form, form_html) {
+      form.html(form_html);
+      $('#'+form.attr('id')+' form').on('submit', function(event) {
+        event.preventDefault();
+        form.select('form #submit-id-submit')
+          .attr('disabled', 'disabled')
+          .attr('value','Processing...')
+          .classed({'processing': true});
+        var core_form = '#'+form.attr('id')+' form';
+        $.ajax({
+          url: "/guest/add/",
+          type: "POST",
+          data: $(core_form).serialize(),
+          success: function(data) {
+            if (!(data['success'])) {
+              _displayForm(d3.select(core_form), data['form_html']);
+            }
+            else {
+              console.log(form);
+              form.remove();
+            }
+          },
+          error: function () {
+            $(core_form).find('.error-message').show()
+          }
+        });
+      });
+
     }
 
     var _requestGuestForm = function () {
@@ -11,13 +50,14 @@ var Confirm = (function () {
         type: 'GET',
         url: '/guest/add/',
         success: function (data) {
-          _form_html = data['form_html'];
-          _initForm();
+          _form_html = data;
+          addForm();
         },
       })
     };
 
     var initialize = function () {
+      _confirm_container = d3.select('.confirmations-container');
       _requestGuestForm();        
     }
 
